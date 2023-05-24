ee29b2352b44abacf45da7e83faed61f0b9c1532
Dwene Louis <Dwenegt@yahoo.com>
2023-05-24 15:50:26 -0400

    Update 3

--- src/main/java/com/repoone/Main.java
+++ src/main/java/com/repoone/Main.java
@@ -2,6 +2,6 @@ package com.repoone;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Humans!");
+        System.out.println("The planet will be destroyed in 5 miniutes");
     }
 }
